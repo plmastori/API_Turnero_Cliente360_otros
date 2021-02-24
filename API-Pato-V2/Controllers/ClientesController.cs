39e6505e84d7c0347763c2e80c8e9e32e6729889
plmastori <patri.mastori@gmail.com>
2021-02-24 14:00:26 -0300

    Clientes Controller

---
+++ API-Pato-V2/Controllers/ClientesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_Pato_V2.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        //++ADD - 1. Interfaz de información del Cliente
+        public List<sp_api_VISTA_360_CLIENTE_Result> Get(string dni)
+        {
+            using (BI_PRODEntities te = new BI_PRODEntities())
+            {
+                var t = te.sp_api_VISTA_360_CLIENTE(dni);
+                return t.ToList();
+            }
+        }
+
+    }
+}
